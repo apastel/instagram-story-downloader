4e788763cbe52c5d110addb4cdc32d946e5be2f9
Alex Pastel <alex.r.pastel@gmail.com>
2020-02-28 18:19:35 -0800

    initial commit of python module

---
+++ story_fetcher.py
@@ -0,0 +1,16 @@
+from instaloader import Instaloader, Profile
+import yaml
+
+L = Instaloader(dirname_pattern='data/{target}', save_metadata=False, download_video_thumbnails=False)
+config = yaml.safe_load(open('config.yaml'))
+
+for user in config['users']:
+    try:
+        L.load_session_from_file(user, f'{user}.session')
+    except:
+        L.interactive_login(user)
+    
+    L.save_session_to_file(f'{user}.session')
+
+    profiles = [Profile.from_username(L.context, username) for username in config['users'][user]['profiles']]
+    L.download_profiles(profiles, profile_pic=False, posts=False, stories=True, fast_update=True)
