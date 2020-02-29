d2348bb99cc22b6810160473e028b00909b66a0c
apastel <alex.r.pastel@gmail.com>
2020-02-29 13:48:27 -0800

    Put sessions under new directory. Added script to run program

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from instaloader import Instaloader, Profile
 import yaml
+import os
 
 L = Instaloader(dirname_pattern='data/{target}', save_metadata=False, download_video_thumbnails=False)
 config = yaml.safe_load(open('config.yaml'))
 
+if not os.path.exists('session'):
+    os.mkdir('session')
+
 for user in config['users']:
     try:
-        L.load_session_from_file(user, f'data/{user}.session')
+        L.load_session_from_file(user, f'session/{user}.session')
     except:
         L.interactive_login(user)
     
-    L.save_session_to_file(f'data/{user}.session')
+    L.save_session_to_file(f'session/{user}.session')
 
     profiles = [Profile.from_username(L.context, username) for username in config['users'][user]['profiles']]
     L.download_profiles(profiles, profile_pic=False, posts=False, stories=True, fast_update=True)
---
+++ run_me.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source /home/pi/.instagram-story-downloader/bin/activate
+cd /home/pi/instagram-story-downloader
+python main.py
+deactivate
+
+
