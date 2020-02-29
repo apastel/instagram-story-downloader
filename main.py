cfdb3f4ea5c904e161e1f33423f91b549dd9fea9
Alex Pastel <alex.pastel@moesol.com>
2020-02-29 11:41:14 -0800

    Update filenames and gitignore

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ config = yaml.safe_load(open('config.yaml'))
 
 for user in config['users']:
     try:
-        L.load_session_from_file(user, f'{user}.session')
+        L.load_session_from_file(user, f'data/{user}.session')
     except:
         L.interactive_login(user)
     
-    L.save_session_to_file(f'{user}.session')
+    L.save_session_to_file(f'data/{user}.session')
 
     profiles = [Profile.from_username(L.context, username) for username in config['users'][user]['profiles']]
     L.download_profiles(profiles, profile_pic=False, posts=False, stories=True, fast_update=True)
