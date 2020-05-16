03a94019fd8bee385642e3def939354ee989676f
apastel <alex.r.pastel@gmail.com>
2020-05-16 15:24:20 -0700

    Download posts if specified in config

--- main.py
+++ main.py
@@ -8,20 +8,21 @@ config = yaml.safe_load(open('config.yaml'))
 if not os.path.exists('session'):
     os.mkdir('session')
 
-for user in config['users']:
+for account in config['accounts']:
     try:
-        L.load_session_from_file(user, f'session/{user}.session')
+        L.load_session_from_file(account, f'session/{account}.session')
     except:
-        L.interactive_login(user)
+        L.interactive_login(account)
     
-    L.save_session_to_file(f'session/{user}.session')
+    L.save_session_to_file(f'session/{account}.session')
 
-    # Download profiles at a time in case one of them has a problem, it doesn't crash the whole process
-    for username in config['users'][user]['profiles']:
-        print(f'\n=== {username} ===')
+    # Download profiles one at a time in case one of them has a problem, it doesn't crash the whole process
+    for user in config['accounts'][account]:
+        print(f"\n=== {user['profile']} ===")
         try:
-            profile = Profile.from_username(L.context, username)
-            L.download_profiles([profile], profile_pic=False, posts=False, stories=True, fast_update=True)
+            profile = Profile.from_username(L.context, user['profile'])
+            dl_posts = 'posts' in user and user['posts']
+            L.download_profiles([profile], profile_pic=False, posts=dl_posts, stories=True, fast_update=True)
         except Exception as e:
             print(e)
 
